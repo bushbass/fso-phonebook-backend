6d6b95d78213d588eab673569c89805d128b254a
Alex Nielsen <alex@bigal.org>
2024-03-27 10:09:06 -0400

    start at 3.5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -43,6 +43,11 @@ app.get('/api/persons/:id', (req, res) => {
     console.log({ person })
     if (person) { res.json(person) }
     else { res.status(404).end() }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    persons = persons.filter(person => person.id !== Number(req.params.id))
+    res.status(204).end()
 })
 
 const PORT = 3001
