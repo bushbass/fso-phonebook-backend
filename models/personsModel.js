7913179bf7e4fa5771e776149b7ecad2462918f8
Alex Nielsen <alex@bigal.org>
2024-04-16 13:50:08 -0400

    updated dist

--- models/personsModel.js
+++ models/personsModel.js
@@ -30,7 +30,7 @@ const personSchema = new mongoose.Schema({
             validator: function (v) {
                 return /\d{3}-\d{3}-\d{4}/.test(v);
             },
-            message: props => `${props.value} is not a valid phone number!`
+            message: props => `${props.value} is not a valid phone number`
         }, required: true
     },
 })
